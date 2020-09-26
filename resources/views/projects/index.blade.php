5c999587866535aececc40c0849232faf5f87856
alexsnowb <mne@lexa.in>
2020-09-26 23:59:24 +0300

    Modify project header

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,13 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Projects') }}
-        </h2>
-        <div>
-            <a href="/dashboard/projects/create" class="text-blue-500 hover:text-blue-800" >
-                {{ __('Create') }}
-            </a>
+        <div class="flex">
+            <div class="w-5/6">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __('Projects') }}
+                </h2>
+            </div>
+            <div class="w-1/6 justify-end text-right">
+                <a href="/dashboard/projects/create" class="items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150  justify-end" >
+                    {{ __('Create') }}
+                </a>
+            </div>
         </div>
+
     </x-slot>
 
     <div>
