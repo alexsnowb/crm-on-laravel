47eb16c8f6ba8c22f988cf8f1701276341715072
alexsnowb <mne@lexa.in>
2020-10-08 23:28:12 +0300

    Update task

--- app/Http/Livewire/Project/Task/Index.php
+++ app/Http/Livewire/Project/Task/Index.php
@@ -11,9 +11,32 @@ class Index extends Component
 {
     use WithPagination;
 
+    /**
+     * @var Project
+     */
     public $project;
 
+    /**
+     * @var string
+     */
     public $newTaskTitle;
+
+    /**
+     * @var string
+     */
+    public $updatedTaskTitle;
+
+    /**
+     * @var Task[]
+     */
+    private $tasks;
+
+
+    /**
+     * @var bool
+     */
+    private $taskUpdated;
+
 
     public function addNewTask()
     {
@@ -31,15 +54,39 @@ class Index extends Component
 
     }
 
-    public function render()
+    public function updateTask($taskId)
     {
-        $tasks = Task::where('project_id', $this->project->id)
+        /** @var Task $task */
+        $task = Task::findOrFail($taskId);
+        $task->title = $this->updatedTaskTitle;
+        $task->save();
+
+        $this->tasks = Task::where('project_id', $this->project->id)
             ->orderBy('id', 'desc')
             ->paginate(10)
         ;
 
+        session()->flash('message', 'Task updated successfully');
+
+
+        $this->updatedTaskTitle = '';
+
+
+    }
+
+    public function render()
+    {
+
+        if( empty($this->tasks) ) {
+            $this->tasks = Task::where('project_id', $this->project->id)
+                ->orderBy('id', 'desc')
+                ->paginate(10)
+            ;
+        }
+
+
         return view('livewire.project.task.index', [
-            'tasks' => $tasks
+            'tasks' => $this->tasks
         ]);
     }
 }
